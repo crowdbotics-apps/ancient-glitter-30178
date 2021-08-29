2c2d576b19ee4ca67c027c21eeea4ce9d5f7e7a9
Crowdbotics <team@crowdbotics.com>
2021-08-29 11:22:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ancient_glitter_30178Identifier",
+  url: "https://ancient_glitter_30178Identifier.botics.co",
+  api: "https://ancient_glitter_30178Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
